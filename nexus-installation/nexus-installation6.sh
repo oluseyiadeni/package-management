b42e9fb9be2942120d3022cd1ce092dc986b8bc6
Dayo Ade <112366888+oluseyiadeni@users.noreply.github.com>
2022-12-31 13:51:02 -0600

    Update nexus-installation6.sh

--- nexus-installation/nexus-installation6.sh
+++ nexus-installation/nexus-installation6.sh
@@ -39,8 +39,7 @@ sudo chmod -R 775 /opt/sonatype-work
 
 #6 Open /opt/nexus/bin/nexus.rc file and  uncomment run_as_user parameter and set as nexus user.
 
-vi /opt/nexus/bin/nexus.rc
-run_as_user="nexus"
+echo  'run_as_user="nexus" ' > /opt/nexus/bin/nexus.rc
 
 #7 CONFIGURE NEXUS TO RUN AS A SERVICE 
 
